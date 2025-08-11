a0dcff48691f1e6b02429b6473e140dd269f7232
dplochcoder <daniel.ploch.coder@gmail.com>
2025-08-11 02:58:14 -0400

    Implement deathtimer subcommand

--- Modules/PauseModule/CountdownManager.cs
+++ Modules/PauseModule/CountdownManager.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TheHuntIsOn.Modules.PauseModule;
+﻿namespace TheHuntIsOn.Modules.PauseModule;
 
 internal class CountdownManager
 {
